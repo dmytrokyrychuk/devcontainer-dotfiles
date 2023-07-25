09318399918cffad79f922e993b9607ed782c87c
Dmytro Kyrychuk <dmytro@kyrych.uk>
2023-07-25 11:18:18 +0000

    Add fish, starship and fzf

--- home.nix
+++ home.nix
@@ -2,4 +2,17 @@
   programs.home-manager.enable = true;
   home.packages = [
   ];
+
+  programs.fzf.enable = true;
+  programs.starship.enable = true;
+  programs.starship.settings = {
+    container.disabled = true;
+  };
+
+  programs.fish = {
+    enable = true;
+    interactiveShellInit = ''
+      set fish_greeting  # Disable greeting
+    '';
+  };
 }
