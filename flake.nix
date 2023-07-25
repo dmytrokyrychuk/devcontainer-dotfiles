a9de47a1b2dbe9109d277a4551c88b510e5a6081
Dmytro Kyrychuk <dmytro@kyrych.uk>
2023-07-25 13:21:28 +0000

    Clean up vars

--- flake.nix
+++ flake.nix
@@ -5,15 +5,12 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
     home-manager.url = "github:nix-community/home-manager/release-23.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
-    vars.url = "path:./vars";
   };
 
   outputs = {
     self,
     nixpkgs,
     home-manager,
-    vars,
     ...
   } @ inputs: let
     system = "x86_64-linux";
