f879e5bebad9ad23c514c05b9e0fa931c89549ed
Dmytro Kyrychuk <dmytro@flowtale.ai>
2024-02-14 16:48:41 +0000

    Upgrade inputs to NixOS 23.11

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "VSCode Dev Container dotfiles.";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
-    home-manager.url = "github:nix-community/home-manager/release-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
